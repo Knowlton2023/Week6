720a03126cd30e581afd61436c6077287b20f8c5
Charles Knowlton <clknowlton@icloud.com>
2023-10-21 15:33:03 -0500

    WIP

--- src/com/coderscampus/lesson1/Teacher.java
+++ src/com/coderscampus/lesson1/Teacher.java
@@ -1,7 +1,25 @@
 package com.coderscampus.lesson1;
 
 public class Teacher {
+	private String classes;
+	private String levalOfEducation;
 
-	
+	public Teacher () {
+		this.classes = "No classes assigned";
+		this.levalOfEducation = "No education assigned";
+	}
+
+	public String getClasses() {
+		return classes;
+	}
+	public void setClasses(String classes) {
+		this.classes = classes;
+	}
+	public String getLevalOfEducation() {
+		return levalOfEducation;
+	}
+	public void setLevalOfEducation(String levalOfEducation) {
+		this.levalOfEducation = levalOfEducation;
+	}
 
 }
