7dc70f8c2ec9e9cb4377ca4eae037f93c11ab025
Charles Knowlton <clknowlton@icloud.com>
2023-11-11 19:14:01 -0600

    WIP

--- src/com/coderscampus/lesson8/FileOutputApplication.java
+++ src/com/coderscampus/lesson8/FileOutputApplication.java
@@ -1,9 +1,18 @@
 package com.coderscampus.lesson8;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class FileOutputApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		BufferedWriter write = new BufferedWriter(new FileWriter("data.txt"));
 		
+		
+		BufferedReader reader = new BufferedReader(new FileReader("data.txt"));
 
 	}
 
